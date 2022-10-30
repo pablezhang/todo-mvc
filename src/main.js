a0a6b4c6df940d7bb7a0a6b15a1cb12b233fd93a
luckybo0027 <luckybo0027@gmail.com>
2022-10-30 17:23:33 +0800

    chore: 清除vue相关

--- src/main.js
+++ src/main.js
@@ -1,9 +1,1 @@
-import { createApp } from 'vue';
-import './index.css';
-import './style.css';
-import App from './App.vue';
-import { createPinia } from 'pinia';
-
-const pinia = createPinia();
-
-createApp(App).use(pinia).mount('#app');
+console.log('everything is ok  ----->  ');
